2c908d6068fc4ca8b2da641aab176228455674ef
Eduardo <eduardohairescampos@gmail.com>
2025-03-20 15:51:01 -0300

    fix: error not list jobs

--- src/schemas/job.schemas.ts
+++ src/schemas/job.schemas.ts
@@ -4,9 +4,7 @@ import { userSchema } from "./user.schemas";
 export const jobSchema = z.object({
   id: z.number().optional(),
   title: z.string().min(3, "O título da vaga deve ter pelo menos 3 caracteres"),
-  description: z
-    .string()
-    .min(5, "A descrição deve ter pelo menos 10 caracteres"),
+  description: z.string(),
   isActive: z.boolean().default(true), 
   createdAt: z.date().default(new Date()),
   users: z.array(userSchema).optional(),
