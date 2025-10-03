1410575fb19bb4ed713e63f530f35777137cbf3d
Eduardo <eduardohairescampos@gmail.com>
2025-10-02 21:37:35 -0300

    feat: Replace Nodemailer with Resend for email delivery

--- src/services/sendEmail.services.ts
+++ src/services/sendEmail.services.ts
@@ -1,29 +1,37 @@
+// src/services/sendEmail.services.ts
+
+import { Resend } from "resend";
 import {
   ApplicationEmailData,
   ContactEmailData,
 } from "../interface/aplication.interfaces";
-import transporter from "../config/nodemailer.config";
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const fromEmail = "onboarding@resend.dev";
+const toEmail = process.env.EMAIL_USER
+  ? process.env.EMAIL_USER
+  : "Hex@playthedarkwest.com";
 
 export const sendApplicationEmail = async (
   data: ApplicationEmailData
 ): Promise<{ success: boolean; message?: string; error?: any }> => {
   const { name, email, message, portfolioLink, jobName } = data;
 
-  const mailOptions = {
-    to: "Hex@playthedarkwest.com",
-    subject: `Application for job name: ${jobName}`,
-    text: `  
-      Name: ${name}  
-      E-mail: ${email}  
-      Portfolio: ${portfolioLink || "Not provided"}   
-      Message: ${message}  
-
-      Application for job Name: ${jobName}  
-    `,
-  };
-
   try {
-    await transporter.sendMail(mailOptions);
+    await resend.emails.send({
+      from: fromEmail,
+      to: toEmail,
+      subject: `Application for job name: ${jobName}`,
+      text: `
+        Name: ${name}
+        E-mail: ${email}
+        Portfolio: ${portfolioLink || "Not provided"}
+        Message: ${message}
+        Application for job Name: ${jobName}
+      `,
+    });
     return { success: true, message: "Email sent successfully!" };
   } catch (error) {
     return { success: false, error };
@@ -35,20 +43,18 @@ export const sendContactEmail = async (
 ): Promise<{ success: boolean; message?: string; error?: any }> => {
   const { name, email, message, portfolioLink } = data;
 
-  const mailOptions = {
-    to: "Hex@playthedarkwest.com",
-    subject: "Contact Form Message",
-    text: `  
-      Name: ${name}  
-      E-mail: ${email}
-      Portfolio: ${portfolioLink || "Not provided"}      
-      Message: ${message}  
-      
-    `,
-  };
-
   try {
-    await transporter.sendMail(mailOptions);
+    await resend.emails.send({
+      from: fromEmail,
+      to: toEmail,
+      subject: "Contact Form Message",
+      text: `
+        Name: ${name}
+        E-mail: ${email}
+        Portfolio: ${portfolioLink || "Not provided"}
+        Message: ${message}
+      `,
+    });
     return { success: true, message: "Contact email sent successfully!" };
   } catch (error) {
     return { success: false, error };
@@ -58,20 +64,18 @@ export const sendContactEmail = async (
 export const getGameEmail = async (
   data: ContactEmailData
 ): Promise<{ success: boolean; message?: string; error?: any }> => {
-  const { name, email, message, portfolioLink } = data;
-
-  const mailOptions = {
-    to: "Hex@playthedarkwest.com",
-    subject: "Game Form Message",
-    text: `  
-    ${name.charAt(0).toUpperCase() + name.slice(1)} has joined the hunters!  
-    
-    E-mail: ${email}  
-  `,
-  };
+  const { name, email } = data;
 
   try {
-    await transporter.sendMail(mailOptions);
+    await resend.emails.send({
+      from: fromEmail,
+      to: toEmail,
+      subject: "Game Form Message",
+      text: `
+        ${name.charAt(0).toUpperCase() + name.slice(1)} has joined the hunters!
+        E-mail: ${email}
+      `,
+    });
     return { success: true, message: "Contact email sent successfully!" };
   } catch (error) {
     return { success: false, error };
